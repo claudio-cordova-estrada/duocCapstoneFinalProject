84ad33baae31f2d702bd5143129d24ef023dae7f
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2026-05-07 19:18:25 -0400

    checkpoint: antes de rediseño UI con mockup oscuro

--- Proyecto/planificApp/obj/Release/net10.0/planificApp.AssemblyInfo.cs
+++ Proyecto/planificApp/obj/Release/net10.0/planificApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ede47b6f413afb15a7513f795b3e4c54f62ad6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637f46e673717279c51fac2485b058173e0a51a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
