b73edfc244eb1340f89edce557aa868c57d4f3fd
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2026-06-06 11:24:37 -0400

    debí de haber cerrado antes de hacer el commit type commit

--- Proyecto/planificApp/obj/Release/net10.0/planificApp.AssemblyInfo.cs
+++ Proyecto/planificApp/obj/Release/net10.0/planificApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789ef0f630763a20933a3800e5b277147b560505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fc702e40748092daae4c9e7f3ec5f35988af3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
