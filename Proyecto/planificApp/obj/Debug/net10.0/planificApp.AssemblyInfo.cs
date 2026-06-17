767682a90aaee2d5278bc876aa76a5057b4fd2da
IanFCortes <ia.cortes@duocuc.cl>
2026-06-16 22:56:51 -0400

    comienzo de generacion semanal
    falta arreglar bugs de vistas ADMIN

--- Proyecto/planificApp/obj/Debug/net10.0/planificApp.AssemblyInfo.cs
+++ Proyecto/planificApp/obj/Debug/net10.0/planificApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32293a9c2192d81bbb0b81e334986ed95fc25a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c843d23f3c86b66ebd3e6645ff042ff56ee98a7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("planificApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
